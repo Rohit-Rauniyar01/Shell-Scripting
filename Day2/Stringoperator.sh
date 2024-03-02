b4b4158fbeecc062bd891ad4c66dc6f03e61769b
Rohit Rauniyar <rohitgupta2019email@gmail.com>
2024-03-02 13:28:54 +0530

    modified

---
+++ Day2/Stringoperator.sh
@@ -0,0 +1,8 @@
+#!usr/bin/zsh
+#here we gar going to learn about the String Operator in shell scripting
+
+myvar="Hello World! Please let me know"
+myVarlen="${#myvar}" # we can count the length of the string in the Array
+echo "Length of my variable is $myVarlen"
+echo "$myVarlen"
+
