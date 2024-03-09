8a9d941df1bc3a69f2f62d6d09f13a044a2c0c82
developer <developer@kali.developer>
2024-03-09 14:05:11 +0530

    Dub k mar ja

---
+++ Day3/cond.sh
@@ -0,0 +1,34 @@
+#!usr/bin/zsh # this is known as shebang line
+
+# if-else in shell scripting 
+
+read -p "Enter your mark " marks
+if [[ $marks -gt 40 ]]
+then
+    echo "You are pass 
+    Enjoy Your day $marks"
+
+    else
+    echo "Dub k mar ja $marks"
+
+fi
+
+
+# if elif else in shell scripting 
+
+read -p "Enter your mark " marks
+if [[ $marks -ge 85 ]]
+then
+    echo "1st Division"
+    elif
+    [[ $marks -ge 65 ]]
+    then 
+    echo "2nd Division"
+    elif
+    [[ $marks -ge 40 ]]
+    then
+    echo "3rd Division" 
+    else
+    echo "Dub k mar ja $marks"
+
+fi
