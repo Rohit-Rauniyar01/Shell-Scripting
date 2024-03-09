12a98c7240bca15bfc4c75f05bcc2cea4536ff31
developer <developer@kali.developer>
2024-03-09 12:55:32 +0530

    After Slicing

--- Day2/Stringoperator.sh
+++ Day2/Stringoperator.sh
@@ -9,4 +9,17 @@ echo "$myVarlen"
 echo "upper case is ${myvar^^}" # '^^' used  to print upper-case in Array
 echo "upper case is ${myvar,,}" # ',,' used  to print lower-case in Array
 
+# Here we can change the myvar string or can replace the word
+newVar=${myvar/World/Duniya}
+echo "New var is ----------$newVar"
+
+# to slice the index from myvar in shell scripting
+
+echo "After Slicing ---     ${myvar:6:6}"
+
+<<comment
+'myvar' is the name of the variable from which the substring will be extracted.
+The first '6' after the colon ':' indicates the starting position of the substring you want to extract. In Bash, string indexing starts at 0, so 6 means the extraction starts from the 7th character of the string contained in myvar.
+The second '6' specifies the length of the substring to be extracted, meaning that 6 characters starting from the specified position (inclusive) will be taken from 'myvar'.
+comment
 
